eadc6e047db89a9ef3b507539d2de034954a650f
ambrwlsn <arw89@uclive.ac.nz>
2017-12-06 10:48:22 +0000

    JS functionality added - delete button appends to each <li> and hides <li> when clicked. Tidied and commented JS

--- public/js/script.js
+++ public/js/script.js
@@ -1,25 +1,10 @@
-// function B() {
-//     document.getElementById("display").innerHTML =
-//     "<h1 class='list-title'>"
-//     + document.getElementById('title').value + "</h1>"
-//     + "<br><br>"
-//     + "Recipient: "
-//     + document.getElementById('recipient').value
-//     + "<br>" + "Link: "
-//     +  document.getElementById('link').value
-//     + "<br>"
-//     + "Price: "
-//     + document.getElementById('price').value;
-// }
-
-var title = document.getElementById("title").value;
-var recipient = document.getElementById("recipient").value;
-var link = document.getElementById("link").value;
-var price = document.getElementById("price").value;
-
+// Creates a new list item when clicking on the "Add" button
 function showNewGift(){
     var li = document.createElement("li");
-    // var inputArray = [title, recipient, link, price];
+
+// var inputArray = [title, recipient, link, price]; ~ haven't figured out how to tidy the code by utilising arrays yet
+
+// Places each user-inputted value into a variable
     var titleValue = document.getElementById("title").value;
     var recValue = document.getElementById("recipient").value;
     var imgValue = document.getElementById("link").value;
@@ -30,75 +15,36 @@ function showNewGift(){
     var showInput = document.createTextNode(strValue);
     li.appendChild(showInput);
 
+// Creates alert box if either of the title or recipient inputs are empty
     if (titleValue === '' | recValue === '') {
       alert("You must add a gift and recipient!");
     } else {
       document.getElementById("display").appendChild(li);
     }
-    document.getElementById("titleValue").value = "";
-  }
+    // document.getElementById("titleValue").value = "";
 
-function emptyInput() {
-    if (inputValue === '')
-    alert("You must write something!");
-}
+// Appends 'x' unicode character as a delete button to the DOM
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
 
-function deleteInput(){
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
     }
   }
+
+// Click on a close button to hide the current list item
+// !But this doesn't delete the generated <li></li> in the DOM! ~ how can I do this? do I even need to do this, or is it ok to leave those <li> there?
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
 }
-
-// var div = document.getElementById("display");
-// var textNode = document.createTextNode("my content");
-// div.appendChild(textNode)
-// document.body.appendChild(div);
-
-// function () {
-//
-// }
-// var results = function B(){};
-// let colorboxMarkup = '';
-//
-// results.forEach(function(item) {
-// var arrayLength = item.colors.length;
-//     for (var i = 0; i < arrayLength; i++) {
-//         console.log(item.colors[i]);
-//         colorboxMarkup += `<div class='mini--box' style="background:#${item.colors[i]};"></div>`
-// }
-
-// function create_gift_markup(gift) {
-//       const markup =`
-//           <p>
-//               <span class=''> ${display.title} </span><br>
-//               <span class=''> ${display.recipient} </span><br>
-//               <span class=''> ${display.link} </span><br>
-//               <span class=''> ${display.price} </span><br>
-//           </p>`;
-//       return markup;
-//   }
-//
-//   function insert_gifts(gift_markup) {
-//       const gift_fragment = document.createDocumentFragment();
-//       gift_markup.forEach(function __append_markup_to_docfrag(markup) {
-//           let container_element = document.createElement('span');
-//           container_element.insertAdjacentHTML('afterbegin', markup);
-//           gift_fragment.appendChild(container_element.firstElementChild);
-//       })
-//       document.body.appendChild(gift_fragment);
-//   }
-//
-//   function create_list_markup(gifts) {
-//           const gift_markup = create_gift_markup(gifts)
-//           // insert the created markup into our DOM as children of the <body> element
-//           insert_gifts(gift_markup)
-//       }
