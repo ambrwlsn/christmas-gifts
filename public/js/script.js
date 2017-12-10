34fcd3cee0fbd7d2d4001b8f8b5d330f2091227f
ambrwlsn <arw89@uclive.ac.nz>
2017-12-10 21:33:51 +0000

    reordered code, rename functions, added pseudo code

--- public/js/script.js
+++ public/js/script.js
@@ -1,62 +1,32 @@
-var form = document.getElementById('gift_form');
-form.addEventListener("submit", showNewGift, false);
+var form = document.getElementById('gift_submit_form');
+form.addEventListener("submit", addNewGiftToGiftList, false);
 
-function showNewGift(event) {
+function addNewGiftToGiftList(event) {
     event.preventDefault();
     emptyInputAlert();
-
     insertGiftDisplayMarkup();
     allowGiftEdit();
     clearUserInputFields();
 }
 
-function allowGiftEdit() {
-   // get the last gift item in the list
-    var gifts = document.getElementsByClassName("edit_gift");
-    var lastItem = gifts.length - 1;
-    // attach a click event to the  button
-    // which changes it into editing mode
-    gifts[lastItem].addEventListener("click", giftEditEvent, false);
-}
-
-function giftEditEvent(giftEvent) {
-    giftEvent.preventDefault();
-
-    var edited_gift = editedGiftContents();
-    var giftItemForm = giftEvent.target.parentNode;
-    var inputWrapper = giftItemForm.getElementsByClassName("gift_item_data");
-    var editedInput = editedInputHTML(edited_gift);
-
-    for (var i = 0, length = inputWrapper.length; i < length; i++) {
-    inputWrapper[i].insertAdjacentHTML('beforeend', editedInput);
-  }
-}
-
-function editedGiftContents() {
-    return {
-        title: document.getElementsByClassName("title_span").value,
-        recipient: document.getElementsByClassName("recipient_span").value,
-        link: document.getElementsByClassName("link_span").value,
-        price: document.getElementsByClassName("price_span").value,
-    };
-}
-
-function editedInputHTML(edited_gift){
-  const markup = `
-  <input type="text" value="${edited_gift.title}" />`;
-  return markup;
-}
+function emptyInputAlert(){
+    var $titleValue = document.getElementById("title").value;
+    var $recValue = document.getElementById("recipient").value;
+    if ($titleValue.trim() === '' | $recValue.trim() === '') {
+      alert("You must add a gift and recipient!");
+    } else {
+      false
+    } 
+ }
 
 function insertGiftDisplayMarkup(){
-    var gift = getUserInputGiftContents();
-    var giftMarkup = generateGiftDisplayMarkup(gift);
-    var containerElement = document.getElementById("display");
-
-    containerElement.insertAdjacentHTML('beforeend', giftMarkup);
+    var $container_element = document.getElementById("gift_list_section"); //targets gift list section (a div container in the HTML file)
+    var new_gift = giftSubmitFormUserInput(); //accesses user input in gift submit form and puts it in 'new_gift' variable
+    var gift_markup = generateGiftDisplayMarkup(new_gift); //inserts user input (using new_gift variable) into HTML template strings 
+    $container_element.insertAdjacentHTML('beforeend', gift_markup); //targets gift list section, runs insert adjacent HTML function & places the user + HTML template strings into the gift list section
 }
 
-// This function contains object literals - Object literals encapsulate data, enclosing it in a tidy package - http://www.dyn-web.com/tutorials/object-literal/#syntax
-function getUserInputGiftContents() {
+function giftSubmitFormUserInput() {
     return {
         title: document.getElementById("title").value,
         recipient: document.getElementById("recipient").value,
@@ -65,14 +35,13 @@ function getUserInputGiftContents() {
     };
 }
 
-// This function contains a template literal which, in this case, allows embedded HTML - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 function generateGiftDisplayMarkup(gift) {
-    const markup = `
+    const $markup = `
           <form class="gift_item">
 
              <div class="gift_item_data">
                <label>Title: </label>
-               <span class="title_span" style="float: right;">${gift.title}</span><br>
+               <span class="title_span" style="float: right;">${gift.title}</span>
              </div>
              <div class="gift_item_data">
                <label>Recipient: </label>
@@ -88,143 +57,66 @@ function generateGiftDisplayMarkup(gift) {
              </div>
              <button name="button" class="edit_gift">Edit</button>
          </form>`;
-    return markup;
+    return $markup;
 }
 
-// This function is placed in the submit function (showNewGift) to clear the user input fields once submit has been fired
 function clearUserInputFields() {
-     document.getElementById("title").value = "";
-     document.getElementById("recipient").value = "";
-     document.getElementById("link").value = "";
-     document.getElementById("price").value = "";
+    document.getElementById("title").value = "";
+    document.getElementById("recipient").value = "";
+    document.getElementById("link").value = "";
+    document.getElementById("price").value = "";
 }
 
-// This function is placed in the submit function (showNewGift) to create an alert if the title or recipient values are not entered once submit has been fired
-function emptyInputAlert(){
-    var titleValue = document.getElementById("title").value;
-    var recValue = document.getElementById("recipient").value;
-    var imgValue = document.getElementById("link").value;
-    var priceValue = document.getElementById("price").value;
-    if (titleValue.trim() === '' | recValue.trim() === '') {
-      alert("You must add a gift and recipient!");
-    } else {
-      false
-    }
+function clickEdit(giftEvent) {
+    giftEvent.preventDefault();
+
+    var edited_gift = giftSubmitFormUserInput();
+    var $gift_item_form = giftEvent.target.parentNode;
+    var $input_wrapper = $gift_item_form.getElementsByClassName("gift_item_data");
+    var editedInput = editedInputHTML(edited_gift);
+
+    $gift_item_form.insertAdjacentHTML('beforeend', editedInput);
+
+//     for (var i = 0, length = input_wrapper.length; i < length; i++) {
+//         input_wrapper[i].insertAdjacentHTML('beforeend', editedInput);
+//   }
 }
 
-//When the user clicks the edit button, I want each span with user input (in a given gift block) to transform into an input.
-//I want the user to be able to edit the input
-//I want the user to be able to click a save button to save their altered input
+function allowGiftEdit() {
+    var $gifts = document.getElementsByClassName("edit_gift");
+    var lastItem = $gifts.length - 1;
+    $gifts[lastItem].addEventListener("click", clickEdit, false);
+} // a dev at the unit wrote this function. he's nice but talks really fast and I'd didn't full understand what it does
 
-// function deleteButton(){ // Appends 'x' unicode character as a delete button to the DOM
-//     var span = document.createElement("button");
-//     var txt = document.createTextNode("\u00D7");
-//     span.className = "close";
-//     span.appendChild(txt);
-//     li.appendChild(span);
-//     // Click on a close button to not only hide but delete current list item
-//     var close = document.getElementsByClassName("close");
-//         for (i = 0; i < close.length; i++) {
-//           close[i].onclick = function() {
-//             this.parentNode.parentNode.removeChild(this.parentNode);
-//         }
-//     }
-// }
+function editedGiftContents() {
+    return {
+        title: document.getElementsByClassName("title_span").value,
+        recipient: document.getElementsByClassName("recipient_span").value,
+        link: document.getElementsByClassName("link_span").value,
+        price: document.getElementsByClassName("price_span").value,
+    };
+}
 
-// function createButtonElement(li){
-//     var li = document.createElement("li");
-//     var att = document.createAttribute("contenteditable");
-//     att.value = "true";
-//     li.setAttributeNode(att);
-//     document.getElementsByClassName("display").appendChild(li);
-// }
+function editedInputHTML(edited_gift){
+  const markup = `
+  <input type="text" class="edit_input_here" value="${edited_gift.title}" />
+  <input type="text" class="edit_input_here" value="${edited_gift.recipient}" />
+  <input type="text" class="edit_input_here" value="${edited_gift.link}" />
+  <input type="text" class="edit_input_here" value="${edited_gift.price}" />`;
+  return markup;
+} // I can't figure out what to write in the value attribute here. What is currently there does not work
 
-//***************************************************************************************//
+                            //***!!! START PSUEDO CODE !!!***/
 
+// I want the user to click the edit button and each of 4 fields becomes an input field
+// I want the edit button to turn to a save button right away, so that the edit function can't run anymore (and create extra input fields)
 
-// function showNewGift(){
-//     var titleValue = document.getElementById("title").value;
-//     var recValue = document.getElementById("recipient").value;
-//     var imgValue = document.getElementById("link").value;
-//     var priceValue = document.getElementById("price").value;
-// // Creates alert box if either of the title or recipient inputs are empty
-//     if (titleValue.trim() === '' | recValue.trim() === '') {
-//       alert("You must add a gift and recipient!");
-//     } else {
-//       var gift_display = generateGiftDisplayText(gift);
-//       document.getElementById("list-box").appendChild(gift_display);
-//     }
-//
-//     function generateGiftDisplayText(gift) {
-//         const markup = `
-//                <form id="display">
-//                     <label> Title: ${gift.title} </label><br>
-//                     <label> Recipient: ${gift.recipient} </label><br>
-//                     <label> Link: ${gift.link} </label><br>
-//                     <label> Price: ${gift.price} </label><br>
-//                 </form>`;
-//         return markup;
-//       }
-//   }
+// I want the user to be able to click the save button to store the new input value, 
 
+// I want a cancel button to be available in case the user clicked edit but decided they don't want to edit the input
 
-//***************************************************************************************//
+                            //***!!! END PSUEDO CODE !!!***/
 
+// The way I tried to do this so far is creating a loop (commented out above in clickEdit function) that will inject an input element (from the editedInputHTML function). I then decided to try injecting 4 input elements instead (from the editedInputHTML function) that appears below the original user input
 
-// // Creates a new list item when clicking on the "Add" button
-// function showNewGift(){
-//     // var li = document.createElement("li");
-//
-//     var li = document.createElement("li");
-//     var att = document.createAttribute("contenteditable");
-//     att.value = "true";
-//     li.setAttributeNode(att);
-//
-// //Array containing each input
-//     var inputArray = [title, recipient, link, price];
-//
-// // Places each user-inputted value into a variable
-//     var titleValue = document.getElementById("title").value;
-//     var recValue = document.getElementById("recipient").value;
-//     var imgValue = document.getElementById("link").value;
-//     var priceValue = document.getElementById("price").value;
-//
-//     var strValue = titleValue + " for: " + recValue + " with this great pic: " + imgValue + " that costs this much: " + priceValue;
-//
-//     var showInput = document.createTextNode(strValue);
-//     li.appendChild(showInput);
-//
-// // Creates alert box if either of the title or recipient inputs are empty
-//     if (titleValue.trim() === '' | recValue.trim() === '') {
-//       alert("You must add a gift and recipient!");
-//     } else {
-//       document.getElementById("display").appendChild(li);
-//     }
-// // Empties the input fields once submit button is pressed
-//     // document.getElementById("titleValue").value = "";
-//     var form = document.getElementById("gift_form");
-//     form.reset();
-//
-// // Appends 'x' unicode character as a delete button to the DOM
-//     var span = document.createElement("button");
-//     var txt = document.createTextNode("\u00D7");
-//     span.className = "close";
-//     span.appendChild(txt);
-//     li.appendChild(span);
-//
-// // Appends edit button to the DOM
-//     var span = document.createElement("button");
-// // ~ How can I style the edit text?
-//     var txt = document.createTextNode(" edit");
-//     span.className = "edit";
-//     span.appendChild(txt);
-//     li.appendChild(span);
-//
-// // Click on a close button to not only hide but delete current list item
-// var close = document.getElementsByClassName("close");
-//     for (i = 0; i < close.length; i++) {
-//       close[i].onclick = function() {
-//         this.parentNode.parentNode.removeChild(this.parentNode);
-//       }
-//     }
-//   }
+// I'm not happy with how I approached this so far. I only want a simple solution (such as here https://gist.github.com/asciidisco/ae8afd34b055a1cc8cedce2feb7f86d3). The creator of this code simply used Label elements (no inputs) and changed readOnly from true to false. I would have done this too, but to be honest, the way that code links with the CSS really confuses me, as does a lot of the syntax (because I am sooooo new and got no grammar ;) If my German was as good as my JS, all I'd be able to say would be 'Ich bin ein Berliner').
